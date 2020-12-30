1a6708f935ec826c4d070c3ddb54cfa374316780
Danila Sokolov <matsubus@mail.ru>
2020-12-30 14:59:28 +0300

    feat(covnerter): completed converter proj

--- script/script.js
+++ script/script.js
@@ -4,33 +4,67 @@ class CurrencyConverter {
         this.formElements = [...this.form.elements];
         this.inputElements = this.formElements.filter(item => item.type === 'text');
         this.radioElements = this.formElements.filter(item => item.type === 'radio');
+        this.spanItems = form.querySelectorAll('span');
+        this.revertFlag = false;
+        this.eventsHandler();
     }
 
-    init() {
-        this.eventsHandler();
-    };
-
     eventsHandler() {
+        this.form.addEventListener('submit', event => {
+            event.preventDefault();
+            this.convert();
+        });
 
+        this.form.addEventListener('change', event => {
+            const target = event.target;
+            if (target.type === "radio" && target.matches('[name=convert]')) {
+                this.changeCurrency(target);
+            } else if (target.type === "checkbox" && target.matches('[name=revert]')) {
+                this.revert();
+            }
+        });
+
+        this.inputElements.forEach(item => {
+            item.addEventListener('input', () => {
+                console.log('here');
+                item.value = item.value.replace(/[^\d\.]/, '');
+            });
+        })
     };
 
     revert() {
-
+        [this.spanItems[0].textContent, this.spanItems[1].textContent] = 
+            [this.spanItems[1].textContent, this.spanItems[0].textContent]
+        this.revertFlag = !this.revertFlag;
     };
 
-    checkVallet() {
-
-    };
+    changeCurrency(radio) {
+        if (this.revertFlag) this.spanItems[1].textContent = radio.value;
+        else this.spanItems[0].textContent = radio.value;
+    }
 
     convert() {
+        const myCurrency = this.form.querySelector(`.${this.inputElements[0].id}`),
+            convertCurrency = this.form.querySelector(`.${this.inputElements[1].id}`);
+        const body = {
+            'from': myCurrency.textContent,
+            'to': convertCurrency.textContent,
+            'amount': +this.inputElements[0].value
+        };  
+        
+        this.apiRequest(body)
+            .then(response => {
+                if (response.status !== 200) {
+                    throw new Error('error');
+                };
 
-    };
+                return response.json()
+            })
+            .then(data => this.inputElements[1].value = data.rates[body['to']] * body.amount);
+    }
 
-    apiRequest() {
-
-    };
+    apiRequest = body => fetch('https://api.exchangeratesapi.io/latest' + '?base=' + body.from);
 }
 
 const mainForm = document.querySelector('.main-form');
 const converter = new CurrencyConverter(mainForm);
-converter.init();
